8bdde63ce8e23cb0aba47877e9ecc14c05d4023b
MiriamJimenezIesBelen <163839552+MiriamJimenezIesBelen@users.noreply.github.com>
2026-02-08 16:54:41 +0100

    Proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {RegistroEmpresaComponent} from './components/registro-empresa/registro-empresa';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegistroEmpresaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/models/empresa.model.ts
@@ -0,0 +1,10 @@
+export interface Empresa {
+  idEmpresa?: number;
+  numeroRegistro: string;
+  nombre: string;
+  sector: string;
+  pais: string;
+  ciudad: string;
+  tamano: string;
+  correoContacto: string;
+}
